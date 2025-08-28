f5842c1f6c94255b81980cac7bdffb60c9673bd6
Andrew <dipshitdumbest@gmail.com>
2025-08-28 00:49:21 -0400

    Fixed the atribute system

--- test.py
+++ test.py
@@ -10,46 +10,39 @@ fighter_name = ["Joe", "Derek", "Edwin", "Donut-Eater", "Adrian", "Bart", "Xiang
 def get_name():
         name = random.choice(fighter_name)
         return name
+
 #Generates a random number from 0 to 10 for all atributes
 def gen_atr():
       atr = random.randint(0,10)
       return atr
 
+
 #This class contains every stat of the fighters
-class fighter:
-        #These allow functions to pass through as a vaild values
-        def name(name):
-            return name
+class Fighter:
+     def __init__(self):
+          self.name = get_name()
+          self.str = gen_atr()
+          self.spd = gen_atr()
+          self.agi = gen_atr()
+          self.con = gen_atr()
+          self.int = gen_atr()
+          self.size = gen_atr()
 
-        def str(atr):
-             return atr
-        
-        def spd(atr):
-             return atr
-        
-        def agi(atr):
-             return atr
-        
-        def int(atr):
-             return atr
-        
-        def size(atr):
-             return atr
-        
-        def con(atr):
-             return atr
-        
-
-       
-
-            
+     def stat_check(self):
+          print("Fighter's name is "+str(self.name))
+          print("Strength = "+str(self.str))
+          print("Speed = "+str(self.spd))
+          print("Agilty = "+str(self.agi))
+          print("Toughness = "+str(self.con))
+          print("Intelligence = "+str(self.int))
+          print("Size = "+str(self.size))    
 
 def fighter_menu():
-    fighter1 = fighter
-    fighter2 = fighter 
-    print("1. "+fighter1.name(get_name()))
-    print("2. "+fighter2.name(get_name()))
-    print("Please select your fighter")
+    fighter1 = Fighter()
+    fighter2 = Fighter()
+    print("1. "+fighter1.name)
+    print("2. "+fighter2.name)
+    print("Please select your fighter(1 or 2)")
 
 def start():
     print("Welcome to Gambling Sim!.")
